149e2099ed6f8ff6a652075804cceaa24ead1acd
leidy lopez <leydytata@gmail.com>
2026-05-18 21:24:54 -0500

    feat: HU-001 registro de usuario completado

---
+++ app/core/constants.py
@@ -0,0 +1,22 @@
+# app/core/constants.py
+
+class ErrorCodes:
+    """Códigos de error del sistema"""
+    EMAIL_EXISTS = "EMAIL_EXISTS"
+    INVALID_DATA = "INVALID_DATA"
+    INVALID_EMAIL = "INVALID_EMAIL"
+    INVALID_PASSWORD = "INVALID_PASSWORD"
+    INVALID_PHONE = "INVALID_PHONE"
+    USER_NOT_FOUND = "USER_NOT_FOUND"
+    UNDERAGE_USER = "UNDERAGE_USER"
+
+class HttpStatus:
+    """Códigos HTTP"""
+    OK = 200
+    CREATED = 201
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    CONFLICT = 409
+    INTERNAL_ERROR = 500
